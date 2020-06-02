57d6e660e1783b84df2d9c13fd225aef89997648
Blogem <jochem@jochemvanleeuwen.nl>
2020-06-02 10:57:10 +0200

    Replace tabs with spaces

--- flatten_the_curve/data_dumper/data_dumper.py
+++ flatten_the_curve/data_dumper/data_dumper.py
@@ -81,32 +81,32 @@ def read_covid(dir):
                 insert_q = '''INSERT INTO covid(file,report_date,province_state,country_region,last_update,confirmed,
                     death,recovered,lat,lon) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
 
-			elif(file.startswith('03-22') or
-					file.startswith('03-23') or
-					file.startswith('03-24') or
-					file.startswith('03-25') or
-					file.startswith('03-26') or
-					file.startswith('03-27') or
-					file.startswith('03-28') or
-					file.startswith('03-29') or
-					file.startswith('03-30') or
-					file.startswith('03-31') or
-					file.startswith('04-') or
-					file.startswith('05-0') or
-					file.startswith('05-1') or
-					file.startswith('05-20') or
-					file.startswith('05-21') or
-					file.startswith('05-22') or
-					file.startswith('05-23') or
-					file.startswith('05-24') or
-					file.startswith('05-25') or
-					file.startswith('05-26') or
-					file.startswith('05-27') or
-					file.startswith('05-28')):
-				insert_q = '''INSERT INTO covid(file,report_date,province_state,country_region,last_update,lat,lon,confirmed,
+            elif(file.startswith('03-22') or
+                    file.startswith('03-23') or
+                    file.startswith('03-24') or
+                    file.startswith('03-25') or
+                    file.startswith('03-26') or
+                    file.startswith('03-27') or
+                    file.startswith('03-28') or
+                    file.startswith('03-29') or
+                    file.startswith('03-30') or
+                    file.startswith('03-31') or
+                    file.startswith('04-') or
+                    file.startswith('05-0') or
+                    file.startswith('05-1') or
+                    file.startswith('05-20') or
+                    file.startswith('05-21') or
+                    file.startswith('05-22') or
+                    file.startswith('05-23') or
+                    file.startswith('05-24') or
+                    file.startswith('05-25') or
+                    file.startswith('05-26') or
+                    file.startswith('05-27') or
+                    file.startswith('05-28')):
+                insert_q = '''INSERT INTO covid(file,report_date,province_state,country_region,last_update,lat,lon,confirmed,
                     death,recovered,active,combined_key) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
 
-			else:
+            else:
                 insert_q = 'INSERT INTO covid VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
 
             covidcsv_to_table(dir,file,c,insert_q)
